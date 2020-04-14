d8165401b3622f49daab6dcb8490f3f497ad9988
Servais DD NKOUKA <servaisnkouka6@gmail.com>
2020-04-14 01:14:58 +0100

    Login Composant: System d'authentification initialisé

--- client/src/views/Pages/Login/Login.js
+++ client/src/views/Pages/Login/Login.js
@@ -2,6 +2,27 @@ import React, { Component } from 'react';
 import { Button, Card, CardBody, Col, Container, Form, Input, InputGroup, InputGroupAddon, InputGroupText, Row } from 'reactstrap';
 
 class Login extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      emailuser: '',
+      password: ''
+    }
+    
+    this.onChange = this.onChange.bind(this);
+    this.login = this.login.bind(this);
+  }
+
+  onChange(e) {    
+    this.setState({[e.target.name]: [e.target.value] });    
+  }
+
+  login(e) {
+    // Intégration API        
+  }
+
   render() {
     return (
       <div className="app flex-row align-items-center">
@@ -10,16 +31,17 @@ class Login extends Component {
             <Col md="5">              
                 <Card className="p-4">
                   <CardBody>
-                    <Form>
+                    <Form onSubmit={(values) => this.login(values)}>
                       <h1>Connecter</h1>
                       <p className="text-muted">Connectez vous à votre compte</p>
                       <InputGroup className="mb-3">
                         <InputGroupAddon addonType="prepend">
                           <InputGroupText>
-                            <i className="icon-user"></i>
+                            <i className="icon-envelope"></i>
                           </InputGroupText>
                         </InputGroupAddon>
-                        <Input type="text" placeholder="Nom utilisateur" autoComplete="username" />
+                        <Input type="email" placeholder="E-mail"
+                                name="emailuser" onChange={this.onChange} />
                       </InputGroup>
                       <InputGroup className="mb-4">
                         <InputGroupAddon addonType="prepend">
@@ -27,14 +49,16 @@ class Login extends Component {
                             <i className="icon-lock"></i>
                           </InputGroupText>
                         </InputGroupAddon>
-                        <Input type="password" placeholder="Mot de passe" autoComplete="current-password" />
+                        <Input type="password" placeholder="Mot de passe" 
+                              autoComplete="current-password"
+                              name="password" onChange={this.onChange} />
                       </InputGroup>
                       <Row>
                         <Col xs="6">
-                          <Button color="danger" className="px-4">Login</Button>
+                          <Button color="primary" className="px-4" onClick={this.login} >Se connecter</Button>
                         </Col>
                         <Col xs="6" className="text-right">
-                          <Button color="link" className="px-0">Forgot password?</Button>
+                          <Button color="link" className="px-0">Mot de passe oublié ?</Button>
                         </Col>
                       </Row>
                     </Form>
